d3b5b98f29b731fd70b3a9ad00a43cb06135ad15
Matheus Duarte <matheusduartegalvao@gmail.com>
2021-02-23 22:16:42 -0300

    First message

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.listen(3333, () => console.log('Server us running!'));
+app.get('/', (request, response) => {
+  return response.json({message: 'teste man'});
+});
+
+app.post('/', (request, response) => {
+  return response.json({message: 'Inseriu essa miséria.'});
+});
+
+app.listen(3333, () => console.log("Server us running!"));
